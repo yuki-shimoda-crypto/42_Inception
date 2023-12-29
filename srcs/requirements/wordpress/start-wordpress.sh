687820b6cdcef2d65286b7fa02901439ecea0b60
yuki-shimoda <yuki-shimoda@protonmail.com>
2023-12-29 13:18:43 +0900

    WP USERをenvで指定できるようにした

--- srcs/requirements/wordpress/start-wordpress.sh
+++ srcs/requirements/wordpress/start-wordpress.sh
@@ -3,8 +3,8 @@
 # WordPressのインストール状況をチェック
 if ! wp core is-installed --allow-root --path=/var/www/html; then
     # WordPressが未インストールの場合、インストールを実行
-    wp core install --url=yshimoda.42.fr --title="WordPress Site" --admin_user=satoshi --admin_password=password --admin_email=admin@example.com --path=/var/www/html --allow-root
-    wp user create yshimoda yshimoda@example.com --user_pass=password --role=editor --allow-root
+    wp core install --url=yshimoda.42.fr --title="WordPress Site" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --path=/var/www/html --allow-root
+    wp user create ${WP_USER} ${WP_EMAIL} --user_pass=${WP_PASSWORD} --role=editor --allow-root
     wp option update timezone_string 'Asia/Tokyo'
 fi
 
